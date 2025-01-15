2bca1d916ccc93e4a4a1df08358668d79f5e6031
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2025-01-15 06:22:51 +0000

    Implement mapping existing jobs to markup

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -2,7 +2,15 @@ import { useState } from "react";
 import ExperienceForm from "./ExperienceForm";
 
 const WorkExperience = () => {
-  const [previousJobs, setPreviousJobs] = useState([]);
+  const [previousJobs, setPreviousJobs] = useState([
+    {
+      id: 0,
+      company: "kint",
+      position: "manager",
+      timePeriod: "2017-2022",
+      responsibilities: "lol",
+    },
+  ]);
   const [formVisible, setFormVisible] = useState(false);
   const [status, setStatus] = useState("typing");
 
@@ -13,6 +21,14 @@ const WorkExperience = () => {
   return (
     <>
       <h3 className="pb-2 text-lg font-medium">Work experience</h3>
+      {previousJobs.map((job) => (
+        <div key={job.id} className="company mb-4 flex flex-col gap-1">
+          <h4 className="text-base font-medium">{job.company}</h4>
+          <h5>{job.position}</h5>
+          <em>{job.timePeriod}</em>
+          <p>{job.responsibilities}</p>
+        </div>
+      ))}
       <button onClick={handleAdd} type="button">
         Add new
       </button>
