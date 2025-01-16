d221b11f1bfd71f998d9fa2cc321098fa5b5cf81
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2025-01-16 05:26:52 +0000

    Add Delete job experience button and its event handling

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -23,7 +23,26 @@ const WorkExperience = () => {
       <h3 className="pb-2 text-3xl font-medium">Work experience</h3>
       {previousJobs.map((job) => (
         <div key={job.id} className="company mb-4 flex flex-col gap-1">
-          <h4 className="text-2xl font-medium">{job.company}</h4>
+          <div className="flex gap-4">
+            <h4 className="text-2xl font-medium">{job.company}</h4>
+            <ul className="flex gap-2">
+              <li>
+                <button className="bg-transparent p-0 text-2xl">🖉</button>
+              </li>
+              <li>
+                <button
+                  className="bg-transparent p-0 text-2xl"
+                  onClick={() => {
+                    setPreviousJobs(
+                      previousJobs.filter((j) => j.id !== job.id),
+                    );
+                  }}
+                >
+                  🗑
+                </button>
+              </li>
+            </ul>
+          </div>
           <h5 className="text-lg">{job.position}</h5>
           <em>{job.timePeriod}</em>
           <p className="text-sm">{job.responsibilities}</p>
@@ -32,7 +51,9 @@ const WorkExperience = () => {
       <button onClick={handleAdd} type="button">
         Add new
       </button>
-      {formVisible && <ExperienceForm></ExperienceForm>}
+      {formVisible && (
+        <ExperienceForm /*onSubmit={handleSubmit}*/></ExperienceForm>
+      )}
       {/*<div className="former-job">
         <h4>Employer&apos;s Name</h4> 
         
